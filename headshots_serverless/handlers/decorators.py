fd6a4d5942fdb0d50c9e46116da13bf75c62f66b
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-05-11 22:37:20 -0700

    more loggers

--- headshots_serverless/handlers/decorators.py
+++ headshots_serverless/handlers/decorators.py
@@ -1,5 +1,6 @@
 import logging
 from functools import wraps
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -8,8 +9,18 @@ logger.setLevel(logging.INFO)
 def log_event(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        logger.info(msg=args)
-        logger.info(msg=kwargs)
+        logger.info("#EVENT")
+        logger.info(msg=args[0])
+        return f(*args, **kwargs)
+
+    return wrapper
+
+
+def log_environment(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        logger.info("#ENVIRONMENT")
+        logger.info(msg=os.environ)
         return f(*args, **kwargs)
 
     return wrapper
