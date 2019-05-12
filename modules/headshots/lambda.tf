dbfca104a436a24c617987dba02aeef7e4cd0ea2
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-05-12 12:55:53 -0700

    try to fix import error

--- modules/headshots/lambda.tf
+++ modules/headshots/lambda.tf
@@ -33,7 +33,7 @@ resource "aws_lambda_function" "hello_lambda" {
   source_code_hash = "${data.archive_file.handlers.output_base64sha256}"
 
   role    = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "hello.handler"
+  handler = "handlers.hello.handler"
   runtime = "python3.6"
 }
 
