18e843faf9f96f232321d86abadc28cca2f9b49f
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-05-11 20:30:39 -0700

    return explicit creation of cloudwatch logs

--- modules/headshots/cloudwatch.tf
+++ modules/headshots/cloudwatch.tf
@@ -1,3 +1,10 @@
+# This is to optionally manage the CloudWatch Log Group for the Lambda Function.
+# If skipping this resource configuration, also add "logs:CreateLogGroup" to the IAM policy below.
+resource "aws_cloudwatch_log_group" "hello_lambda_log" {
+  name              = "/aws/lambda/${aws_lambda_function.hello_lambda.function_name}"
+  retention_in_days = 14
+}
+
 # See also the following AWS managed policy: AWSLambdaBasicExecutionRole
 resource "aws_iam_policy" "lambda_logging" {
   name = "lambda_logging"
