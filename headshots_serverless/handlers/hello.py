51bea8ffab3dd862c278fdd7f872998ee32e9195
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-05-11 20:51:10 -0700

    fix body param

--- headshots_serverless/handlers/hello.py
+++ headshots_serverless/handlers/hello.py
@@ -1,7 +1,7 @@
-import os
-import boto3
 import logging
-import json
+import os
+
+import boto3
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -9,5 +9,5 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     client = boto3.client('s3')
-    client.put_object(Bucket="claytondblythe-headshots-dev", Key="test", Body=json.dump(os.environ['greeting']))
+    client.put_object(Bucket="claytondblythe-headshots-dev", Key="test", Body=os.environ['greeting'])
     return "{} from Lambda!".format(os.environ['greeting'])
