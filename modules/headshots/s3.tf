70b2771d890880c9c8d757f57666e59e15193d98
Clayton Blythe <claytonb@zg03162.zillow.local>
2019-05-11 21:09:01 -0700

    Fix objectcreated event

--- modules/headshots/s3.tf
+++ modules/headshots/s3.tf
@@ -7,7 +7,5 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   lambda_function {
     lambda_function_arn = "${aws_lambda_function.hello_lambda.arn}"
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
   }
 }
